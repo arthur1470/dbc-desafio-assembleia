6a4a13fc4fecc0e6849a355aeb51bbd242824841
Arthur Marques <arthur.l2@hotmail.com>
2023-01-16 03:45:44 -0300

    validacoes de document no caso de uso de cadastro de associado

--- application/src/main/java/br/com/dbccompany/assembleia/application/associate/create/DefaultCreateAssociateUseCase.java
+++ application/src/main/java/br/com/dbccompany/assembleia/application/associate/create/DefaultCreateAssociateUseCase.java
@@ -2,6 +2,8 @@ package br.com.dbccompany.assembleia.application.associate.create;
 
 import br.com.dbccompany.assembleia.domain.associate.Associate;
 import br.com.dbccompany.assembleia.domain.associate.AssociateGateway;
+import br.com.dbccompany.assembleia.domain.exceptions.DomainException;
+import br.com.dbccompany.assembleia.domain.validation.Error;
 
 import java.util.Objects;
 
@@ -15,12 +17,27 @@ public class DefaultCreateAssociateUseCase extends CreateAssociateUseCase {
 
     @Override
     public CreateAssociateOutput execute(final CreateAssociateCommand aCommand) {
-        final var anAssociate = Associate.newAssociate(
-                aCommand.name(),
-                aCommand.document(),
-                aCommand.isActive()
-        );
+        final var aName = aCommand.name();
+        final var aDocument = aCommand.document();
+        final var isActive = aCommand.isActive();
+        final var anAssociate = Associate.newAssociate(aName, aDocument, isActive);
+
+        validateDocument(aDocument);
 
         return CreateAssociateOutput.from(associateGateway.create(anAssociate));
+    }
+
+    private void validateDocument(final String aDocument) {
+        final var isDocumentAlreadyRegistered = associateGateway.existsByDocument(aDocument);
+        if (isDocumentAlreadyRegistered) {
+            final var errorMessage = "'document' %s is already registered".formatted(aDocument);
+            throw DomainException.with(Error.with(errorMessage));
+        }
+
+        final var isDocumentValid = associateGateway.isDocumentValid(aDocument);
+        if (!isDocumentValid) {
+            final var errorMessage = "'document' %s is invalid".formatted(aDocument);
+            throw DomainException.with(Error.with(errorMessage));
+        }
     }
 }
--- domain/src/main/java/br/com/dbccompany/assembleia/domain/associate/AssociateGateway.java
+++ domain/src/main/java/br/com/dbccompany/assembleia/domain/associate/AssociateGateway.java
@@ -2,4 +2,6 @@ package br.com.dbccompany.assembleia.domain.associate;
 
 public interface AssociateGateway {
     Associate create(Associate anAssociate);
+    boolean existsByDocument(String document);
+    boolean isDocumentValid(String document);
 }
