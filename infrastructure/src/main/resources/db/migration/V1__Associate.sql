1a4ded409371b7079de89aef07792a3e07094250
Arthur Marques <arthur.l2@hotmail.com>
2023-01-16 17:05:46 -0300

    Migrations da entidade Associate

---
+++ infrastructure/src/main/resources/db/migration/V1__Associate.sql
@@ -0,0 +1,10 @@
+CREATE TABLE associate
+(
+    id         VARCHAR(36)  NOT NULL PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    document   VARCHAR(11),
+    active     BOOLEAN      NOT NULL DEFAULT TRUE,
+    created_at DATETIME(6)  NOT NULL,
+    updated_at DATETIME(6)  NOT NULL,
+    deleted_at DATETIME(6)
+);
