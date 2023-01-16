27cacd4de54942ded0e57d57ece892c406e935ed
Arthur Marques <arthur.l2@hotmail.com>
2023-01-16 17:05:17 -0300

    Adicionando spring ao projeto, com properties configurados e docker-compose com mysql

---
+++ infrastructure/src/main/java/br/com/dbccompany/assembleia/infrastructure/Main.java
@@ -0,0 +1,14 @@
+package br.com.dbccompany.assembleia.infrastructure;
+
+import br.com.dbccompany.assembleia.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
+    }
+}
---
+++ infrastructure/src/main/java/br/com/dbccompany/assembleia/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.com.dbccompany.assembleia.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.dbccompany.assembleia")
+public class WebServerConfig {
+}
