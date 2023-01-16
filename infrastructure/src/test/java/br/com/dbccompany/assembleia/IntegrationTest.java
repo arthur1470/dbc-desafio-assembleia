885081a8a1ec4dcd57e790e36be1b70fd59e0c35
Arthur Marques <arthur.l2@hotmail.com>
2023-01-16 18:49:47 -0300

    Testes integrados do caso de uso de criar Associate

---
+++ infrastructure/src/test/java/br/com/dbccompany/assembleia/IntegrationTest.java
@@ -0,0 +1,18 @@
+package br.com.dbccompany.assembleia;
+
+
+import br.com.dbccompany.assembleia.infrastructure.configuration.WebServerConfig;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+@SpringBootTest(classes = WebServerConfig.class)
+@ExtendWith(MySQLCleanUpExtension.class)
+public @interface IntegrationTest {
+}
---
+++ infrastructure/src/test/java/br/com/dbccompany/assembleia/application/associate/create/CreateAssociateUseCaseIT.java
@@ -0,0 +1,144 @@
+package br.com.dbccompany.assembleia.application.associate.create;
+
+import br.com.dbccompany.assembleia.IntegrationTest;
+import br.com.dbccompany.assembleia.domain.associate.AssociateGateway;
+import br.com.dbccompany.assembleia.domain.exceptions.DomainException;
+import br.com.dbccompany.assembleia.infrastructure.associate.persistence.AssociateRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@IntegrationTest
+class CreateAssociateUseCaseIT {
+
+    @Autowired
+    private CreateAssociateUseCase useCase;
+    @Autowired
+    private AssociateRepository associateRepository;
+    @SpyBean
+    private AssociateGateway associateGateway;
+
+    @Test
+    void givenAValidCommand_whenCallsCreateAssociate_shouldReturnAssociateId() {
+        final var expectedName = "Joao da Silva";
+        final var expectedDocument = "12345678901";
+        final var expectedIsActive = true;
+
+        final var aCommand = CreateAssociateCommand.with(
+                expectedName,
+                expectedDocument,
+                expectedIsActive
+        );
+
+        assertEquals(0, associateRepository.count());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        assertEquals(1, associateRepository.count());
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
+
+        final var actualAssociate =
+                associateRepository.findById(actualOutput.id()).get();
+
+        assertNotNull(actualAssociate.getId());
+        assertEquals(expectedName, actualAssociate.getName());
+        assertEquals(expectedDocument, actualAssociate.getDocument());
+        assertEquals(expectedIsActive, actualAssociate.isActive());
+        assertNotNull(actualAssociate.getCreatedAt());
+        assertNotNull(actualAssociate.getUpdatedAt());
+        assertNull(actualAssociate.getDeletedAt());
+    }
+
+    @Test
+    void givenAValidCommandWithInactiveAssociate_whenCallsCreateAssociate_shouldReturnInactiveAssociateId() {
+        final var expectedName = "Joao da Silva";
+        final var expectedDocument = "12345678901";
+        final var expectedIsActive = false;
+
+        final var aCommand = CreateAssociateCommand.with(
+                expectedName,
+                expectedDocument,
+                expectedIsActive
+        );
+
+        assertEquals(0, associateRepository.count());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        assertEquals(1, associateRepository.count());
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
+
+        final var actualAssociate =
+                associateRepository.findById(actualOutput.id()).get();
+
+        assertNotNull(actualAssociate.getId());
+        assertEquals(expectedName, actualAssociate.getName());
+        assertEquals(expectedDocument, actualAssociate.getDocument());
+        assertEquals(expectedIsActive, actualAssociate.isActive());
+        assertNotNull(actualAssociate.getCreatedAt());
+        assertNotNull(actualAssociate.getUpdatedAt());
+        assertNotNull(actualAssociate.getDeletedAt());
+    }
+
+    @Test
+    void givenAnInvalidName_whenCallsCreateAssociate_shouldReturnDomainException() {
+        final var expectedName = "  ";
+        final var expectedDocument = "12345678901";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be empty";
+        final var expectedErrorCount = 1;
+
+        final var aCommand = CreateAssociateCommand.with(
+                expectedName,
+                expectedDocument,
+                expectedIsActive
+        );
+
+        assertEquals(0, associateRepository.count());
+
+        final var actualException = assertThrows(
+                DomainException.class,
+                () -> useCase.execute(aCommand)
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(0, associateRepository.count());
+
+        Mockito.verify(associateGateway, Mockito.times(0)).create(any());
+    }
+
+    @Test
+    void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAnException() {
+        final var expectedName = "Joao da Silva";
+        final var expectedDocument = "12345678901";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aCommand = CreateAssociateCommand.with(
+                expectedName,
+                expectedDocument,
+                expectedIsActive
+        );
+
+        assertEquals(0, associateRepository.count());
+
+        Mockito.doThrow(new IllegalStateException("Gateway error"))
+                .when(associateGateway).create(any());
+
+        final var actualException = assertThrows(
+                IllegalStateException.class,
+                () -> useCase.execute(aCommand)
+        );
+
+        assertEquals(0, associateRepository.count());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+}
