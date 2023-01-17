25f316ef7dc93d933c75d6320c449ad52fbc09c9
Arthur Marques <arthur.l2@hotmail.com>
2023-01-17 04:40:55 -0300

    Tratando NotFoundException no Handler Global

--- infrastructure/src/main/java/br/com/dbccompany/assembleia/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/br/com/dbccompany/assembleia/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package br.com.dbccompany.assembleia.infrastructure.api.controllers;
 
 import br.com.dbccompany.assembleia.domain.exceptions.DomainException;
+import br.com.dbccompany.assembleia.domain.exceptions.NotFoundException;
 import br.com.dbccompany.assembleia.domain.validation.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,12 +18,21 @@ public class GlobalExceptionHandler {
         return ResponseEntity.unprocessableEntity().body(ApiError.from(exception));
     }
 
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<?> handleDomainException(final NotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(exception));
+    }
+
     record ApiError(
             String message,
             List<Error> errors
     ) {
         public static ApiError from(final DomainException ex) {
             return new ApiError(ex.getMessage(), ex.getErrors());
+        }
+
+        public static ApiError from(final NotFoundException ex) {
+            return new ApiError(ex.getMessage(), null);
         }
     }
 }
