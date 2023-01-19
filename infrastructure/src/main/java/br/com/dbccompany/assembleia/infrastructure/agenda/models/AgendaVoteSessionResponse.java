22534ef067b5c98eaf570f13072a01769885897e
Arthur Marques <arthur.l2@hotmail.com>
2023-01-19 04:55:48 -0300

    Melhorias na response de buscar Agenda por ID e correcao do teste integrado

--- infrastructure/src/main/java/br/com/dbccompany/assembleia/infrastructure/agenda/models/AgendaVoteSessionResponse.java
+++ infrastructure/src/main/java/br/com/dbccompany/assembleia/infrastructure/agenda/models/AgendaVoteSessionResponse.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Instant;
 
 public record AgendaVoteSessionResponse(
-        @JsonProperty("vote_session_id") String voteSessionId,
-        @JsonProperty("is_vote_session_active") boolean isVoteSessionActive,
-        @JsonProperty("vote_session_started_at") Instant voteSessionStartedAt,
-        @JsonProperty("vote_session_end_at") Instant voteSessionEndAt
+        @JsonProperty("id") String voteSessionId,
+        @JsonProperty("is_active") boolean isVoteSessionActive,
+        @JsonProperty("started_at") Instant voteSessionStartedAt,
+        @JsonProperty("end_at") Instant voteSessionEndAt
 ) {
 }
